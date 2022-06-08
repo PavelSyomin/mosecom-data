d0f8555ff6bc7f145c44514ad3843da6cca80b1b
Pavel Syomin <pas.ntg@gmail.com>
2022-06-08 12:50:28 +0500

    Add Yandexcloud proxy

--- etl/stations.py
+++ etl/stations.py
@@ -4,23 +4,23 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    STATIONS_LIST_URLS = ["https://ru.wikipedia.org",
+    STATIONS_LIST_URLS = ["https://mosecom.mos.ru/stations/",
                           "https://mosecom.mos.ru/specialnye-stancii/"
                         ]
     BASE_URL = "https://mosecom.mos.ru/"
     STATIONS_FOUT_NAME = "stations.txt"
     PROFILERS_FOUT_NAME = "profilers.txt"
+    YACLOUD_PROXY_PREFIX = "https://functions.yandexcloud.net/d4egksn4533obhbontvp"
     stations = []
 
     for station_list_url in STATIONS_LIST_URLS:
-        with urlopen(station_list_url) as con:
+        url = YACLOUD_PROXY_PREFIX + "?url=" + station_list_url
+        with urlopen(url) as con:
             data = con.read().decode("utf8")
 
         if data:
             soup = BeautifulSoup(data, "html.parser")
             stations_list = soup.find("div", id="searching-data")
-            if not stations_list:
-                continue
             for station_div in stations_list.find_all("div", recursive=False):
                 link = station_div.find("a")
                 if link:
