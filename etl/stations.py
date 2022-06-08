3dcb562e501d43b5d3882806118f4baf1e27ef03
Pavel Syomin <pas.ntg@gmail.com>
2022-06-08 11:49:39 +0500

    Add script for fetching the list of stations

---
+++ etl/stations.py
@@ -0,0 +1,48 @@
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
+def main():
+    STATIONS_LIST_URLS = ["https://mosecom.mos.ru/stations/",
+                          "https://mosecom.mos.ru/specialnye-stancii/"
+                        ]
+    BASE_URL = "https://mosecom.mos.ru/"
+    STATIONS_FOUT_NAME = "stations.txt"
+    PROFILERS_FOUT_NAME = "profilers.txt"
+    stations = []
+
+    for station_list_url in STATIONS_LIST_URLS:
+        with urlopen(station_list_url) as con:
+            data = con.read().decode("utf8")
+
+        if data:
+            soup = BeautifulSoup(data, "html.parser")
+            stations_list = soup.find("div", id="searching-data")
+            for station_div in stations_list.find_all("div", recursive=False):
+                link = station_div.find("a")
+                if link:
+                    url = link.get("href")
+                if not url:
+                    print("No URL for station")
+                    continue
+                station_name = url.replace(BASE_URL, "").strip("/")
+                stations.append(station_name)
+        else:
+            print(f"No data for URL {station_list_url}")
+
+    # Add profilemeters manually, because there are only two of them
+    profilers = [
+        "vostok",
+        "ostankino"
+        ]
+
+    with open(STATIONS_FOUT_NAME, "w") as f:
+        f.write("\n".join(stations))
+
+    with open(PROFILERS_FOUT_NAME, "w") as f:
+        f.write("\n".join(profilers))
+
+
+if __name__ == "__main__":
+    main()
