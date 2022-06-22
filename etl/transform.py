e5816c533c445c26d0f611e547fe5742f71d0373
Pavel Syomin <pas.ntg@gmail.com>
2022-06-22 18:11:47 +0500

    Draft code to make csv files from json

---
+++ etl/transform.py
@@ -0,0 +1,112 @@
+import csv
+from datetime import datetime, timedelta, timezone
+import json
+import os
+from collections import namedtuple
+
+
+Params = namedtuple("Params", [
+    "raw_data_dir",
+    "product_dir"
+])
+
+params = Params(
+    raw_data_dir="data/raw",
+    product_dir="data/product"
+)
+
+HEADERS = {
+    "stations": ("datetime", "pollutant", "value"),
+    "special_stations": ("datetime", "pollutant", "value"),
+    "profilers": ("datetime", "height", "temperature")
+}
+
+for dirname, _, filenames in os.walk(params.raw_data_dir):
+    parts = dirname.split("/")
+    if len(parts) != 4:
+        continue
+
+    ptype = parts[2]
+    pname = parts[3]
+
+    out_dir = os.path.join(params.product_dir, ptype, pname)
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    for dtype in ("hourly", "daily", "monthly"):
+        out_file = os.path.join(out_dir, f"rolling_{dtype}.csv")
+        if os.path.isfile(out_file):
+            with open(out_file) as f:
+                reader = csv.reader(f)
+                max_dt = datetime(1970, 1, 1, 0, 0, 1,
+                                  tzinfo=timezone(timedelta(hours=3)))
+                for i, line in enumerate(reader):
+                    if i == 0:
+                        continue
+                    dt = datetime.fromisoformat(line[0])
+                    if dt > max_dt:
+                        max_dt = dt
+
+            rows = set()
+            for filename in sorted(filenames):
+                file_dt = datetime.fromisoformat(filename\
+                                                .rstrip(".json")\
+                                                .split("_", maxsplit=1)[1])
+                if file_dt < max_dt:
+                    continue
+
+                with open(os.path.join(dirname, filename)) as f:
+                    json_data = json.load(f)
+                data = json_data.get("data")
+                if ptype != "profilers":
+                    data = data.get(dtype)
+                if not data:
+                    continue
+                for row in data:
+                    row = tuple(row)
+                    if row[0] is not None:
+                        row_dt = datetime.fromisoformat(row[0])
+                    else:
+                        continue
+                    if row_dt > max_dt:
+                        rows.add(row)
+
+            with open(out_file, "a") as f:
+                writer = csv.writer(f)
+                rows = list(rows)
+                if ptype == "profilers":
+                    rows.sort(key=lambda x: (x[0], x[1] or 0))
+                else:
+                    rows.sort(key=lambda x: x[0])
+                writer.writerows(rows)
+
+        else:
+            rows = set()
+            for filename in filenames:
+                with open(os.path.join(dirname, filename)) as f:
+                    json_data = json.load(f)
+                data = json_data.get("data")
+                if ptype != "profilers":
+                    data = data.get(dtype)
+                if not data:
+                    continue
+                for row in data:
+                    row = tuple(row)
+                    if row[0] is not None:
+                        row_dt = datetime.fromisoformat(row[0])
+                    else:
+                        continue
+                    rows.add(row)
+
+            with open(out_file, "w", encoding="utf8") as f:
+                writer = csv.writer(f)
+                header = HEADERS[ptype]
+                writer.writerow(header)
+                rows = list(rows)
+                if ptype == "profilers":
+                    rows.sort(key=lambda x: (x[0], x[1] or 0))
+                else:
+                    rows.sort(key=lambda x: x[0])
+                writer.writerows(rows)
+
+
