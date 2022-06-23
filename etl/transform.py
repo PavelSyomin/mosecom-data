4d6b4ede40185dc5ec4d4be56e8e445572579a70
Pavel Syomin <pas.ntg@gmail.com>
2022-06-23 17:57:48 +0500

    Add logging to transform.py, get rid of different time variants of profilers data

--- etl/transform.py
+++ etl/transform.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 from datetime import datetime, timedelta, timezone
 import json
 import os
@@ -7,33 +8,52 @@ from collections import namedtuple
 
 Params = namedtuple("Params", [
     "raw_data_dir",
-    "product_dir"
+    "product_dir",
+    "logs_dir",
+    "current_dt"
 ])
 
 params = Params(
     raw_data_dir="data/raw",
-    product_dir="data/product"
+    product_dir="data/product",
+    logs_dir=os.path.join("logs", "transform"),
+    current_dt=datetime.now(tz=timezone(timedelta(hours=3)))\
+            .isoformat(timespec="seconds"),
 )
 
 HEADERS = {
-    "stations": ("datetime", "pollutant", "value"),
-    "special_stations": ("datetime", "pollutant", "value"),
+    "stations": ("datetime", "pollutant", "concentration"),
+    "special_stations": ("datetime", "pollutant", "concentration"),
     "profilers": ("datetime", "height", "temperature")
 }
+
+if not os.path.isdir(params.logs_dir):
+    os.makedirs(params.logs_dir)
+
+logging.basicConfig(
+    filename=os.path.join(params.logs_dir, f"transform_{params.current_dt}.txt"),
+    format="[%(asctime)s] %(levelname)s: %(message)s",
+    level=logging.DEBUG
+)
 
 for dirname, _, filenames in os.walk(params.raw_data_dir):
     parts = dirname.split("/")
     if len(parts) != 4:
         continue
 
-    ptype = parts[2]
-    pname = parts[3]
+    ptype = parts[-2]
+    pname = parts[-1]
 
     out_dir = os.path.join(params.product_dir, ptype, pname)
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
 
-    for dtype in ("hourly", "daily", "monthly"):
+    if ptype in ("stations", "special_stations"):
+        dtypes = ("hourly", "daily", "monthly")
+    else:
+        dtypes = ("every_5_minutes",)
+
+    for dtype in dtypes:
         out_file = os.path.join(out_dir, f"rolling_{dtype}.csv")
         if os.path.isfile(out_file):
             with open(out_file) as f:
@@ -109,4 +129,5 @@ for dirname, _, filenames in os.walk(params.raw_data_dir):
                     rows.sort(key=lambda x: x[0])
                 writer.writerows(rows)
 
+    logging.info(f"Processed {ptype[:-1]} {pname}")
 
